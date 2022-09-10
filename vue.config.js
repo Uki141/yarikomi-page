c65fbfb48f28cfce0abd2bfd8a88cbe3e4ce6743
Yuki Ishii <yuuki141.info@gmail.com>
2022-09-10 18:07:12 +0900

    path修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : './'
 })
